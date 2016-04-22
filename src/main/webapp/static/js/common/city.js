cdb7ae1b086cd6b45298c5dc499ee94102e9470a
lyn@fjsaas.com <lyn@fjsaas.com>
2016-04-22 23:28:00 +0800

    修改了三级联动的调用方式，可在需要的地方，调用province()方法即可

--- src/main/webapp/static/js/common/city.js
+++ src/main/webapp/static/js/common/city.js
@@ -1,37 +1,43 @@
 /******************省市区三级联动***************************************/
-$(function(){
-	//获取省份下拉框
+//获取省份下拉框
+function province(){
 	$('#province').combobox({  
        valueField:'id',  
        textField:'text', 
        method:'get',//设置get，使所有 Web 服务器都应允许（以防出现405错误）
-       url:'../../static/data/province.json',  
+       url:'../static/data/province.json',  
        onSelect:function(data){
     	   getCities(data);//市级
        },
-       onLoadSuccess:function(){
-//    	   alert(1);
+       onLoadSuccess:function(data){
+    		$(this).combobox('setValues', ['--请选择省份--']);
        }
    }); 
+	$('#city').combobox('setValues', ['--请选择城市--']);
+	$('#town').combobox('setValues', ['--请选择城镇--']);
+}
 
-	//获取城市下拉框
-	function getCities(data){
-		$('#city').combobox({  
-		    valueField:'id',  
-		    textField:'text', 
-		    data:data.children,  
-		    onSelect:function(data){
-		    	getTowns(data);
-		    }
-	 }); 
-	}
+//获取城市下拉框
+function getCities(data){
+	$('#city').combobox({  
+	    valueField:'id',  
+	    textField:'text', 
+	    data:data.children,  
+	    onSelect:function(data){
+	    	getTowns(data);
+	    }
+ }); 
+}
 
-	//获取区域下拉框
-	function getTowns(data){
-		$('#town').combobox({  
-		     valueField:'id',  
-		     textField:'text', 
-		     data:data.children
-		}); 
-	}
-});
+//获取区域下拉框
+function getTowns(data){
+	$('#town').combobox({  
+	     valueField:'id',  
+	     textField:'text', 
+	     data:data.children,
+         onLoadSuccess:function(data){
+//        	 alert(JSON.stringify(data));
+    		$(this).combobox('setValues', [data[0].text]);
+         }
+	}); 
+}
