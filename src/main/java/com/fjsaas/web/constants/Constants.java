7d3fe25bbda9f2b3e2329092c2ff543e9431a1b7
lyn@fjsaas.com <lyn@fjsaas.com>
2016-04-26 22:08:31 +0800

    新增一些公共常量

--- src/main/java/com/fjsaas/web/constants/Constants.java
+++ src/main/java/com/fjsaas/web/constants/Constants.java
@@ -4,4 +4,6 @@ public interface Constants {
     String SESSION_FORCE_LOGOUT_KEY = "session.force.logout";
     public String CSV_FILE_URL = "/input/excel/";
     public String CSV_OUTPUT_URL = "/output/excel/";
+    public String IMG_FILE_URL = "/static/image/busslicense/";
+    public String NULL_VALUE = "-999999";
 }
