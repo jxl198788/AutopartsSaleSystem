cfdc4020382e068e2cd59fa6d85705e23679c070
lyn@fjsaas.com <lyn@fjsaas.com>
2016-04-26 22:12:14 +0800

    图片上传实现代码的细节修改

--- src/main/java/com/fjsaas/web/utils/ImageUpload.java
+++ src/main/java/com/fjsaas/web/utils/ImageUpload.java
@@ -14,7 +14,6 @@ import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.io.FilenameUtils;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.fjsaas.web.constants.Constants;
 
@@ -26,30 +25,30 @@ public class ImageUpload {
 	 * request 请求
 	 * filepath 选择图片的本地路径即file文本框中的图片路径
 	 */
-	public static String upload(MultipartFile importImag,HttpServletRequest request,String filepath){
+	public static String upload(HttpServletRequest request,String filepath){
 		String msg = "";
-		String oldFilename = importImag.getOriginalFilename();
+		String oldFilename = filepath.substring(filepath.lastIndexOf("\\")+1, filepath.length());
 		filename = oldFilename;
-		System.out.println("oldFilename:"+oldFilename);
+//		System.out.println("oldFilename:"+oldFilename);
 		String extension = FilenameUtils.getExtension(oldFilename);
-		System.out.println("extension:"+extension);
+//		System.out.println("extension:"+extension);
 
 		if(!"png".equals(extension) && !"jpg".equals(extension)){
 			msg = "上传的图片格式有问题，只支持png、jpg文件";
 		}else{
 			String realPath = request.getSession().getServletContext().getRealPath("/");//获取web项目的路径
-			System.out.println("realPath:"+realPath);
+//			System.out.println("realPath:"+realPath);
 			String fullDir = realPath + Constants.IMG_FILE_URL;
-			System.out.println("fullDir:"+fullDir);
+//			System.out.println("fullDir:"+fullDir);
 			File file = new File(fullDir);
-			System.out.println("文件是否存在:"+file.exists());
+//			System.out.println("文件是否存在:"+file.exists());
 			if(!file.exists()){
 				file.mkdir();//文件不存在就创建
 			}
 			String newFilename = UUID.randomUUID().toString();
-			System.out.println("newFilename:"+newFilename);
+//			System.out.println("newFilename:"+newFilename);
 			String fullPath = fullDir + newFilename + "." + extension;//"D:\\softAnZ\\Workspaces\\Eclipse\\AutopartsSaleSystem\\src\\main\\webapp\\static\\image\\"
-			System.out.println("fullPath:"+fullPath);
+//			System.out.println("fullPath:"+fullPath);
 			try {
 				FileInputStream fis = new FileInputStream(filepath);
 				byte[] b = new byte[1024]; 
